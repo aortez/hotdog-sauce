03741d73109ee0c39f8d8bf0821daba8c8c87cc8
Allan Ortiz <aortez@gmail.com>
2023-10-04 15:48:22 -0700

    load and do basic visualization of mnist_784
    Wasted a bit of time dealing with a bug in the text.
    Found a fix in the forum here:
    https://livebook.manning.com/forum?product=raff&p=1&comment=544786

--- main.py
+++ main.py
@@ -1,64 +1,49 @@
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+import matplotlib.pyplot as pyplot
 import seaborn as sns
+import sklearn
 import timeit
-from tqdm.autonotebook import tqdm
 import torch
-
-# def moveTo(obj, device):
-#     """
-#     obj: the python object to move to the device, or to move its contents to the device
-#     device: the compute device to move the object to
-#     """
-#     if isinstance(obj, list):
-#         return [moveTo(x, device) for x in obj]
-#     elif isinstance(obj, tuple):
-#         return tuple(moveTo(list(obj), device))
-#     elif isinstance(obj, set):
-#         return set(moveTo(list(obj), device))
-#     elif isinstance(obj, dict):
-#         to_ret = dict()
-#         for key, value in obj.items():
-#             to_ret[moveTo(key, device)] = moveTo(value, device)
-#             return to_ret
-#     elif hasattr(obj, "to"):
-#         return obj.to(device)
-#     else:
-#         return obj
+from tqdm.autonotebook import tqdm
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from sklearn.datasets import fetch_openml
 
 
-def f(x):
-    return torch.pow((x - 2.0), 2)
+class SimpleDataset(Dataset):
+
+    def __init__(self, inputs, targets):
+        super(SimpleDataset, self).__init__()
+        self.inputs = inputs
+        self.targets = targets
+
+    def __getitem__(self, index):
+        i = torch.tensor(self.inputs.iloc[index, :], dtype=torch.float32)
+        t = torch.tensor(int(self.targets[index]), dtype=torch.int64)
+        return i, t
+
+    def __len__(self):
+        return self.inputs.shape[0]
 
 
-def f_del(x):
-    return 2*x-4
+def main() -> None:
+
+    # Load MNIST dataset.
+    training_inputs, labels = fetch_openml('mnist_784', version=1, return_X_y=True)
+    print("trainingData.shape:", training_inputs.shape)
+    print("labels.shape:", labels.shape)
+
+    dataset = SimpleDataset(training_inputs, labels)
+    print("dataset.len: ", len(dataset))
+
+    for row in range(10):
+        show_image_and_label(dataset[row][0], dataset[row][1])
+
+
+def show_image_and_label(example, label):
+    pyplot.imshow(example.reshape(28, 28), cmap='gray')
+    pyplot.title("Label: " + str(label))
+    pyplot.show()
 
 
 if __name__ == '__main__':
-    x_axis_vals = np.linspace(-7, 9, 100)
-    y_axis_vals = f(torch.tensor(x_axis_vals)).numpy()
-
-    print("x_axis_vals: ", x_axis_vals)
-    print("y_axis_vals: ", y_axis_vals)
-
-    # Plot pretend loss function.
-    sns.lineplot(x=x_axis_vals, y=y_axis_vals, label="f(x) = (x - 2)^2")
-
-    # Draw line at known minimum.
-    sns.lineplot(x=x_axis_vals, y=[0.0] * len(x_axis_vals), label="minimum", color='black')
-
-    # Compute values of gradient of f(x).
-    y_axis_vals_del = f_del(torch.tensor(x_axis_vals)).numpy()
-
-    # Draw gradient of f(x).
-    sns.lineplot(x=x_axis_vals, y=y_axis_vals_del, label="gradient of $f'(x) = 2x - 4$", color='red')
-
-    # Pretend that we do not know the minimum, and we just look at a bunch of points along the derivative of the loss
-    # function (f(x))). We can see that f(x) < 2 is negative and f(x) > 2 is positive. This tells us how to adjust
-    # our guess at the minimum (which we already know is at x = 2).
-
-    plt.show()
+    main()
---
+++ utils.py
@@ -0,0 +1,101 @@
+import torch
+import matplotlib.pyplot
+import numpy as np
+import pandas as pd
+
+def f(x):
+    return torch.pow((x - 2.0), 2)
+
+
+def f_del(x):
+    return 2*x-4
+
+
+def moveTo(obj, device):
+    """
+    obj: the python object to move to the device, or to move its contents to the device
+    device: the compute device to move the object to
+    """
+    if isinstance(obj, list):
+        return [moveTo(x, device) for x in obj]
+    elif isinstance(obj, tuple):
+        return tuple(moveTo(list(obj), device))
+    elif isinstance(obj, set):
+        return set(moveTo(list(obj), device))
+    elif isinstance(obj, dict):
+        to_ret = dict()
+        for key, value in obj.items():
+            to_ret[moveTo(key, device)] = moveTo(value, device)
+            return to_ret
+    elif hasattr(obj, "to"):
+        return obj.to(device)
+    else:
+        return obj
+
+def do_it():
+    x_axis_vals = np.linspace(-7, 9, 100)
+    y_axis_vals = f(torch.tensor(x_axis_vals)).numpy()
+
+    print("x_axis_vals: ", x_axis_vals)
+    print("y_axis_vals: ", y_axis_vals)
+
+    # Plot pretend loss function.
+    sns.lineplot(x=x_axis_vals, y=y_axis_vals, label="f(x) = (x - 2)^2")
+
+    # Draw line at known minimum.
+    sns.lineplot(x=x_axis_vals, y=[0.0] * len(x_axis_vals), label="minimum", color='black')
+
+    # Compute values of gradient of f(x).
+    y_axis_vals_del = f_del(torch.tensor(x_axis_vals)).numpy()
+
+    # Draw gradient of f(x).
+    sns.lineplot(x=x_axis_vals, y=y_axis_vals_del, label="gradient of $f'(x) = 2x - 4$", color='red')
+
+    # Pretend that we do not know the minimum, and we just look at a bunch of points along the derivative of the loss
+    # function (f(x))). We can see that f(x) < 2 is negative and f(x) > 2 is positive. This tells us how to adjust
+    # our guess at the minimum (which we already know is at x = 2).
+
+    # plt.show()
+
+    x = torch.tensor(2.0, requires_grad=True)
+    print("x.grad: ", x.grad)
+
+    value = f(x)
+    print("value: ", value)
+
+    value.backward()
+    print("x.grad: ", x.grad)
+
+    x = torch.tensor([-3.5], requires_grad=True)
+
+    x_cur = x.clone()
+    x_prev = x_cur * 100
+
+    epsilon = 1e-5
+
+    eta = 0.1
+
+    while torch.linalg.norm(x_cur - x_prev) > epsilon:
+        x_prev = x_cur.clone()
+
+        value = f(x)
+        value.backward()
+        x.data -= eta * x.grad
+
+        x.grad.zero_()
+
+        x_cur = x.data
+
+    print("x_cur: ", x_cur)
+
+    x_param = torch.nn.Parameter(torch.tensor([-3.5]), requires_grad=True)
+
+    optimizer = torch.optim.SGD([x_param], lr=eta)
+
+    for epoch in range(60):
+        optimizer.zero_grad()
+        loss_incurred = f(x_param)
+        loss_incurred.backward()
+        optimizer.step()
+
+    print("x_param: ", x_param)
